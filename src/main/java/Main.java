5a89658c229883cc73511e65b916bc65904bbbb2
Eduard Abrakov <abrakovem@mail.ru>
2019-09-17 09:04:27 +0500

    add class Main

---
+++ src/main/java/Main.java
@@ -0,0 +1,29 @@
+import java.sql.*;
+
+/**
+ * Project JDBC
+ * Created by End on сент., 2019
+ */
+public class Main {
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost:3306/skill";
+        String user = "root";
+        String pass = "parolmysql59";
+        try {
+            Connection connection = DriverManager.getConnection(url,user,pass);
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("select course_name, " +
+                    "count(month(subscription_date))/count(*) as avgPurchase  from purchaselist group by course_name;");
+            while (resultSet.next()) {
+                String courseName = resultSet.getString(1);
+                double avgPurchase = Double.parseDouble(resultSet.getString(2));
+                System.out.println(courseName + " - " + avgPurchase);
+            }
+            connection.close();
+            resultSet.close();
+            statement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
