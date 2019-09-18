514db886b19d6b1ac054857a01c325bdfb217e8d
Eduard Abrakov <abrakovem@mail.ru>
2019-09-18 18:44:58 +0500

    change querry2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,11 +8,14 @@ public class Main {
     public static void main(String[] args) {
         String url = "jdbc:mysql://localhost:3306/skill";
         String user = "root";
-        String pass = "parolmysql59";
+        String pass = "parolmysql159";
         try {
             Connection connection = DriverManager.getConnection(url,user,pass);
             Statement statement = connection.createStatement();
-                ResultSet resultSet = statement.executeQuery("select course_name, count(subscription_date)/6 as avgPurchase from purchaselist group by course_name;");
+                ResultSet resultSet = statement.executeQuery("select course_name, " +
+                        "count(course_name)/(month(max(subscription_date))-month(min(subscription_date))) as avg_per_month " +
+                        "from purchaselist group by course_name;");
+            System.out.println("Курс: - Среднее кол-во покупок в месяц:");
             while (resultSet.next()) {
                 String courseName = resultSet.getString(1);
                 double avgPurchase = Double.parseDouble(resultSet.getString(2));
